1ed9adc1d27334a2f9a6e7cf640f6713ea472991
sivagamasrinivasan <784108@pdsb.net>
2023-05-24 15:26:09 -0400

    version 1 example

---
+++ src/example/Example.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package example;
+
+/**
+ * git versioning, .gitconfig, .gitignore , commit
+ * @author srinivsi
+ */
+public class Example {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("learning github");
+    }
+    
+}
